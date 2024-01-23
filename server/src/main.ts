fb8beddb87c30278758cee5e8949e701723ce91f
CatDeveloper <maks.rozhkov.2004@gmail.com>
2024-01-24 01:41:03 +0500

    fear: add cors module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,14 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import cors from 'cors'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
-    app.enableCors();
+    app.use(cors({
+        credentials: true,
+        origin: true
+    }))
     await app.listen(5000);
 }
 
