a7cefda568f0ef411bc2c367d5ca888687b78cbc
CatDeveloper <maks.rozhkov.2004@gmail.com>
2024-02-19 17:33:23 +0500

    fix: change host

--- frontend/src/api/host.js
+++ frontend/src/api/host.js
@@ -1,4 +1,7 @@
+let HOST;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development")
-    export const HOST = 'http://localhost:5000/api';
+    HOST = 'http://localhost:5000/api';
 else
-    export const HOST = 'https://pincode-backend.ru/api';
+    HOST = 'https://pincode-backend.ru/api';
+
+export default HOST;
